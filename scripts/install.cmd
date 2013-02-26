83199fff35d52b906efc5c26900211db319efe5b
Ramon Smits <ramon@ramonsmits.com>
2013-02-26 22:16:57 +0100

    Added script to install the filter which calls regsvr32

---
+++ scripts/install.cmd
@@ -0,0 +1,9 @@
+REM ===============================================================
+REM http://itboard.ro/blogs/jurnal_de_vet/archive/2011/03/30/x-forwarded-for-pentru-tmg.aspx
+REM ===============================================================
+
+REM Copy filter to TMG application folder
+copy XFF-Filter.dll "C:\Program Files\Microsoft Forefront Threat Management Gateway"
+
+REM registering the DLL with regsvr32 and then filter visible in list TMG console, which can be enabled / disabled:
+regsvr32 XFF-Filter.dll
