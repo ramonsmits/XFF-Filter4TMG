67ab7d8a9f56e96151266bb487d27525fcbf3579
Ramon Smits <ramon@ramonsmits.com>
2013-02-27 17:12:21 +0100

    Added script for signing the installer.

---
+++ scripts/signmsi.cmd
@@ -0,0 +1,1 @@
+signtool sign /sha1 49158C3B8CF083E037D7EF752E0B3FE7237ECEF8 /t http://timestamp.comodoca.com/authenticode /d "Ramon Smits" /du https://github.com/ramonsmits/xff-filter4tmg ..\_build\msi\_Release.x64\xff-filter4tmg.msi
